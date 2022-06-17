8fd5bebfd803b53fc00210de09497053666e9ead
Anna93803 <anna.tassone93@gmail.com>
2022-06-17 16:19:49 +0200

    work in progress

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Descrizione:
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+new Vue({
+    el: "#app",
+    data: {
+        emails: [],
+    },
+    methods: {
+        generationEmail() {
+            for(let i = 0; i < 10; i++) {
+                axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((response) => {
+                        this.emails.push(response.data.response);
+
+                    });
+            }
+        }
+    }
+});
